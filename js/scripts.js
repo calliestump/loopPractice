54e4abfbab52fe61c4765a836e458b594cab4167
Callie Stump <callie@stu.mp>
2020-10-27 13:44:27 -0400

    adds variables to scripts js with puzzle words
    Co-authored-by: Jeff Chiu <jwchiu82@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $("form#wordPuzzle").submit(function() {
+    const epicodus = ["e","p","i","c","o","d","u","s"];
+    const halloween = ["h","a","l","l","o","w","e","e","n"];
+    const coding = ["c","o","d","i","n","g"];
+    const vowels = ["a","e","i","o","u"];
+
+  });
+});
