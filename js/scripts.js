a18842ae3ba8bbdaad3645a984e88c5161748fd4
Callie Stump <callie@stu.mp>
2020-10-27 16:42:53 -0400

    makes all letters autimatically lowercase
    Co-authored-by: Jeff Chiu <jwchiu82@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,14 @@
 $(document).ready(function() {
   $("form#wordPuzzle").submit(function(event) {
     event.preventDefault();
-    //let userWord = [$(".userInput")];
-    const userWord = $("input#userInput").val();
+    const userWord = $("input#userInput").val().toLowerCase();
     const userString = userWord.split("");
-    //console.log(userString);
-    //console.log(userWord);
-    const vowels = ["a","e","i","o","u"];
     for(let i = 0; i < userWord.length; i += 1) {
-      // if (vowels.includes(userWord[index])) // array method
-      //if (userWord[index].includes(vowels)) { // string method
+      userWord.toLowerCase();
       if (userString[i] === "a" || userString[i] === "e" || userString[i] === "i" || userString[i] === "o" || userString[i] === "u") {    
         userString[i] = "-";
       } 
-      //userString.join();
     }
     alert(userString.join(""));
   });
 });
-/*user enters: Apple "-ppl-"
-newUserWord = "";
-if (userWord.charAt(index) = 'a') {
-  let newUserWord += "-";
-
-} */
