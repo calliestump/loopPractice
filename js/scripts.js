071502805d582deba027d83e31f3642fce19f424
Callie Stump <callie@stu.mp>
2020-10-27 16:26:05 -0400

    adds final elements to html and scripts pages
    Co-authored-by: Jeff Chiu <jwchiu82@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,26 @@
 $(document).ready(function() {
-  $("form#wordPuzzle").submit(function() {
-    const epicodus = ["e","p","i","c","o","d","u","s"];
-    const halloween = ["h","a","l","l","o","w","e","e","n"];
-    const coding = ["c","o","d","i","n","g"];
+  $("form#wordPuzzle").submit(function(event) {
+    event.preventDefault();
+    //let userWord = [$(".userInput")];
+    const userWord = $("input#userInput").val();
+    const userString = userWord.split("");
+    //console.log(userString);
+    //console.log(userWord);
     const vowels = ["a","e","i","o","u"];
-
+    for(let i = 0; i < userWord.length; i += 1) {
+      // if (vowels.includes(userWord[index])) // array method
+      //if (userWord[index].includes(vowels)) { // string method
+      if (userString[i] === "a" || userString[i] === "e" || userString[i] === "i" || userString[i] === "o" || userString[i] === "u") {    
+        userString[i] = "-";
+      } 
+      //userString.join();
+    }
+    alert(userString.join(""));
   });
 });
+/*user enters: Apple "-ppl-"
+newUserWord = "";
+if (userWord.charAt(index) = 'a') {
+  let newUserWord += "-";
+
+} */
